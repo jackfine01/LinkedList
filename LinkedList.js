3e527e7e60edced7c977b0d2e9dedeb6ca17fa00
jack <jackfine01@gmail.com>
2025-02-18 07:32:26 -0500

    adding intial append/prepend code

--- LinkedList.js
+++ LinkedList.js
@@ -1,22 +1,43 @@
 class linkedList {
-    constructor(head, size){
+    constructor(head, tail, size){
         this.head = null;
+        this.tail = null;
         this.size = 0;
     }
     append(value){
+        let node = new Node(element);
+        let current;
 
+        if (this.tail == null)
+            this.tail = node;
+        else{
+            current = this.tail;
+        }
+        // move everything up one in index?
+        //increase size
+        this.size++;
     }
     prepend(value){
+        let node = new Node(element);
+        let current;
 
+        if (this.head == null)
+            this.head = node;
+        else{
+            current = this.head;
+        }
+        // move everything down one in index?
+        //increase size
+        this.size++;
     }
     size(){
-
+        return this.size;
     }
     head(){
-
+        return this.head;
     }
     tail(){
-
+        return this.tail;
     }
     at(index){
 
@@ -36,10 +57,10 @@ class linkedList {
 }
 export{linkedList}
 
-class node {
+class Node {
     constructor(value, nextNode) {
         this.value = null;
         this.nextNode = null;
     }
 }
-export{node}
+export{Node}
