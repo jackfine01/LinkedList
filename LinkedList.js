44bfef3b43b42b8ab8d3c1f372ad9aee4c005700
jack <jackfine01@gmail.com>
2025-02-17 22:29:42 -0500

    starting LinkedList Project - Setting up based on instructions.

---
+++ LinkedList.js
@@ -0,0 +1,43 @@
+class linkedList {
+    constructor(){
+
+    }
+    append(value){
+
+    }
+    prepend(value){
+
+    }
+    size(){
+
+    }
+    head(){
+
+    }
+    tail(){
+
+    }
+    at(index){
+
+    }
+    pop(){
+
+    }
+    contains(){
+
+    }
+    find(value){
+
+    }
+    toString(){
+        
+    }
+}
+export{linkedList}
+class node {
+    constructor(value, nextNode) {
+        this.value = null;
+        this.nextNode = null;
+    }
+}
+export{node}
---
+++ main.js
@@ -0,0 +1,10 @@
+import { linkedList } from "LinkedList.js";
+
+const list = new LinkedList();
+
+list.append("dog");
+list.append("cat");
+list.append("parrot");
+list.append("hamster");
+list.append("snake");
+list.append("turtle");
